5736c0429a2420c8f580d8f3ff3cfe2de6108f82
Rohit Gupta <rohitg1594@gmail.com>
2018-08-19 18:58:07 +0200

    add files for deployement

---
+++ deploy/predict.py
@@ -0,0 +1,175 @@
+import random
+import gzip
+from os.path import join
+
+import argparse
+
+from src.data_utils import *
+from src.models.classifier_models import *
+from src.feed_dicts import *
+from src.processing.utils.word_piece_tokenizer import WordPieceTokenizer
+
+from utils import export_predictions
+
+tf.logging.set_verbosity('ERROR')
+
+FLAGS = tf.app.flags.FLAGS
+ENTITY_STRING = 'ENTITY'
+
+THRESHOLD_MAP = {1: 0.3,
+                 2: 0.1,
+                 3: 0.1,
+                 4: 0.5,
+                 5: 0.3,
+                 6: 0.1,
+                 7: 0.1}
+
+parser = argparse.ArgumentParser(description='Script to use bran with PUBMEDID.',
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--bran_dir', type=str, help='path to bran')
+parser.add_argument('--max_sent_len', type=int, help='Maximum number of tokens in a sentence.')
+parser.add_argument('--max_num_sent', type=int, help='Maximum number of sentences in an abstract.')
+parser.add_argument('--pid', type=int, help='PUBMED ID to predict on.')
+
+args = parser.parse_args()
+wpt = WordPieceTokenizer(join(args.bran_dir, 'deploy/bpe.vocab'), entity_str=ENTITY_STRING)
+tokenize = wpt.tokenize
+
+if ('transformer' in FLAGS.text_encoder or 'glu' in FLAGS.text_encoder) and FLAGS.token_dim == 0:
+    FLAGS.token_dim = FLAGS.embed_dim-(2*FLAGS.position_dim)
+
+# read in str <-> int vocab maps
+with open(FLAGS.vocab_dir + '/rel.txt', 'r') as f:
+    kb_str_id_map = {l.split('\t')[0]: int(l.split('\t')[1].strip()) for l in f.readlines()}
+    kb_id_str_map = {i: s for s, i in kb_str_id_map.iteritems()}
+    kb_vocab_size = FLAGS.kb_vocab_size
+with open(FLAGS.vocab_dir + '/token.txt', 'r') as f:
+    token_str_id_map = {l.split('\t')[0]: int(l.split('\t')[1].strip()) for l in f.readlines()}
+    if FLAGS.start_end:
+        if '<START>' not in token_str_id_map:
+            token_str_id_map['<START>'] = len(token_str_id_map)
+        if '<END>' not in token_str_id_map:
+            token_str_id_map['<END>'] = len(token_str_id_map)
+    token_id_str_map = {i: s for s, i in token_str_id_map.iteritems()}
+    token_vocab_size = len(token_id_str_map)
+
+
+with open(FLAGS.vocab_dir + '/entities.txt', 'r') as f:
+    entity_str_id_map = {l.split('\t')[0]: int(l.split('\t')[1].strip()) for l in f.readlines()}
+    entity_id_str_map = {i: s for s, i in entity_str_id_map.iteritems()}
+    entity_vocab_size = len(entity_id_str_map)
+with open(FLAGS.vocab_dir + '/ep.txt', 'r') as f:
+    ep_str_id_map = {l.split('\t')[0]: int(l.split('\t')[1].strip()) for l in f.readlines()}
+    ep_id_str_map = {i: s for s, i in ep_str_id_map.iteritems()}
+    ep_vocab_size = len(ep_id_str_map)
+
+if FLAGS.ner_train != '':
+    with open(FLAGS.vocab_dir + '/ner_labels.txt', 'r') as f:
+        ner_label_str_id_map = {l.split('\t')[0]: int(l.split('\t')[1].strip()) for l in f.readlines()}
+        if FLAGS.start_end:
+            if '<START>' not in ner_label_str_id_map:
+                ner_label_str_id_map['<START>'] = len(ner_label_str_id_map)
+            if '<END>' not in ner_label_str_id_map:
+                ner_label_str_id_map['<END>'] = len(ner_label_str_id_map)
+        ner_label_id_str_map = {i: s for s, i in ner_label_str_id_map.iteritems()}
+        ner_label_vocab_size = len(ner_label_id_str_map)
+else:
+    ner_label_id_str_map = {}
+    ner_label_str_id_map = {}
+    ner_label_vocab_size = 1
+position_vocab_size = (2 * FLAGS.max_seq)
+
+label_weights = None
+if FLAGS.label_weights != '':
+    with open(FLAGS.label_weights, 'r') as f:
+        lines = [l.strip().split('\t') for l in f]
+        label_weights = {kb_str_id_map[k]: float(v) for k, v in lines}
+
+ep_kg_labels = None
+if FLAGS.kg_label_file != '':
+    kg_in_file = gzip.open(FLAGS.kg_label_file, 'rb') if FLAGS.kg_label_file.endswith('gz')\
+        else open(FLAGS.kg_label_file, 'r')
+    lines = [l.strip().split() for l in kg_in_file.readlines()]
+    eps = [('%s::%s' % (l[0], l[1]), l[2]) for l in lines]
+    ep_kg_labels = defaultdict(set)
+    [ep_kg_labels[ep_str_id_map[_ep]].add(pid) for _ep, pid in eps if _ep in ep_str_id_map]
+
+    kg_in_file.close()
+
+e1_e2_ep_map = {}
+ep_e1_e2_map = {}
+
+# get entity <-> type maps for sampling negatives
+entity_type_map, type_entity_map = {}, defaultdict(list)
+if FLAGS.type_file != '':
+    with open(FLAGS.type_file, 'r') as f:
+        entity_type_map = {entity_str_id_map[l.split('\t')[0]]: l.split('\t')[1].strip().split(',') for l in
+                           f.readlines() if l.split('\t')[0] in entity_str_id_map}
+        for entity, type_list in entity_type_map.iteritems():
+            for t in type_list:
+                type_entity_map[t].append(entity)
+        # filter
+        type_entity_map = {k: v for k, v in type_entity_map.iteritems() if len(v) > 1}
+        valid_types = set([t for t in type_entity_map.iterkeys()])
+        entity_type_map = {k: [t for t in v if t in valid_types] for k, v in entity_type_map.iteritems()}
+        entity_type_map = {k: v for k, v in entity_type_map.iteritems() if len(v) > 1}
+
+string_int_maps = {'kb_str_id_map': kb_str_id_map, 'kb_id_str_map': kb_id_str_map,
+                   'token_str_id_map': token_str_id_map, 'token_id_str_map': token_id_str_map,
+                   'entity_str_id_map': entity_str_id_map, 'entity_id_str_map': entity_id_str_map,
+                   'ep_str_id_map': ep_str_id_map, 'ep_id_str_map': ep_id_str_map,
+                   'ner_label_str_id_map': ner_label_str_id_map, 'ner_label_id_str_map': ner_label_id_str_map,
+                   'e1_e2_ep_map': e1_e2_ep_map, 'ep_e1_e2_map': ep_e1_e2_map, 'ep_kg_labels': ep_kg_labels,
+                   'label_weights': label_weights}
+
+word_embedding_matrix = load_pretrained_embeddings(token_str_id_map, FLAGS.embeddings, int(FLAGS.token_dim),
+                                                   token_vocab_size)
+entity_embedding_matrix = load_pretrained_embeddings(entity_str_id_map, FLAGS.entity_embeddings, int(FLAGS.embed_dim),
+                                                     entity_vocab_size)
+
+
+with tf.Graph().as_default():
+    tf.set_random_seed(int(FLAGS.random_seed))
+    np.random.seed(int(FLAGS.random_seed))
+    random.seed(int(FLAGS.random_seed))
+
+    # initialize model
+    if 'multi' in FLAGS.model_type and 'label' in FLAGS.model_type:
+        model_type = MultiLabelClassifier
+    elif 'entity' in FLAGS.model_type and 'binary' in FLAGS.model_type:
+        model_type = EntityBinary
+    else:
+        model_type = ClassifierModel
+    model = model_type(ep_vocab_size, entity_vocab_size, kb_vocab_size, token_vocab_size, position_vocab_size,
+                       ner_label_vocab_size, word_embedding_matrix, entity_embedding_matrix, string_int_maps, FLAGS)
+
+    if FLAGS.load_model != '':
+        reader = tf.train.NewCheckpointReader(join(args.bran_dir, FLAGS.load_model))
+        cp_list = set([key for key in reader.get_variable_to_shape_map()])
+        # if variable does not exist in checkpoint or sizes do not match, dont load
+        r_vars = [k for k in tf.global_variables() if k.name.split(':')[0] in cp_list
+                  and k.get_shape() == reader.get_variable_to_shape_map()[k.name.split(':')[0]]]
+        saver = tf.train.Saver(var_list=r_vars)
+    else:
+        saver = tf.train.Saver()
+    sv = tf.train.Supervisor(logdir=FLAGS.logdir if FLAGS.save_model != '' else None,
+                             global_step=model.global_step,
+                             saver=None,
+                             save_summaries_secs=0,
+                             save_model_secs=0, )
+
+    with sv.managed_session(FLAGS.master,
+                            config=tf.ConfigProto(
+                                # log_device_placement=True,
+                                allow_soft_placement=True
+                            )) as sess:
+        if FLAGS.load_model != '':
+            print("Deserializing model: %s" % FLAGS.load_model)
+            saver.restore(sess, join(args.bran_dir, FLAGS.load_model))
+
+        threads = tf.train.start_queue_runners(sess=sess)
+        fb15k_eval = None
+        tac_eval = None
+        out_file = ''
+        predictions = export_predictions(sess, model, FLAGS, args.pid, string_int_maps, threshold_map=THRESHOLD_MAP)
+        print('Done')
---
+++ deploy/utils.py
@@ -0,0 +1,298 @@
+import sys
+
+import re
+import numpy as np
+import requests
+import time
+
+from nltk.tokenize import sent_tokenize
+
+RE_INT_MATCHER = re.compile('^\d+$')
+RE_FLOAT_MATCHER = re.compile('^\d+\.\d+$')
+ENTITY_STRING = 'ENTITY'
+
+ncbi_url = "https://www.ncbi.nlm.nih.gov/CBBresearch/Lu/Demo/RESTful/tmTool.cgi/BioConcept/"
+
+def equalize_len(data, max_size):
+    l = len(data)
+    if l >= max_size:
+        return data[:max_size]
+    else:
+        for _ in range(max_size - l):
+            data.append(0)
+        return data
+
+
+def parse_flags(flag_file, flags):
+    with open(flag_file, 'r') as f:
+        for line in f:
+            if line.startswith('Loading'):
+                break
+            k, v = line.split(':', 1)
+            k, v = k.strip(), v.strip()
+
+            int_match = RE_INT_MATCHER.match(v)
+            float_match = RE_FLOAT_MATCHER.match(v)
+
+            if k == 'null_label':
+                v = v
+            elif int_match:
+                v = int(v)
+            elif float_match:
+                v = float(v)
+            elif v == 'False':
+                v = False
+            elif v == 'True':
+                v = True
+
+            flags.__dict__[k] = v
+
+    return flags
+
+
+def parse_pubtator(pid, model, tokenize=None, str_id_maps=None, max_sent_len=200):
+    url_submit = ncbi_url + str(pid) + "/PubTator/"
+    try:
+        urllib_result = requests.get(url_submit, timeout=1)
+    except requests.exceptions.Timeout:
+        print('Pubtator URL Not responding, exiting.......')
+        sys.exit(1)
+
+    result = urllib_result.text.strip().split('\n')
+    title = result[0].split('|')[2]
+    abst = result[1].split('|')[2]
+    token_str_id_map = str_id_maps['token_str_id_map']
+    sent_list = []
+    sent_list.append(title)
+    sent_list.extend(sent_tokenize(abst))
+    orig_tokenized_sentences = [tokenize(sent) for sent in sent_list]
+    unequal_tokens = [[token_str_id_map.get(token, 1) for token in sent] for sent in orig_tokenized_sentences]
+    tokens = []
+    for sent in unequal_tokens:
+        tokens.append(equalize_len(sent, max_sent_len))
+    tokens = np.array(tokens)
+
+    seq_len = [len(s) for s in orig_tokenized_sentences]
+    print(seq_len)
+
+    ner_labels, entities, e, e_dist = make_example(result, seq_len)
+
+    final_e1 = []
+    final_e2 = []
+    final_e1_dist = []
+    final_e2_dist = []
+    final_tokens = []
+    final_seq_len = []
+    for sent_idx in range(len(e)):
+        for e1_sent in e[sent_idx]:
+            for e2_sent in e[sent_idx]:
+                final_e1.append(e1_sent)
+                final_e2.append(e2_sent)
+                final_tokens.append(tokens[sent_idx])
+                final_seq_len.append(seq_len[sent_idx])
+
+        for e1_dist_sent in e_dist[sent_idx]:
+            for e2_dist_sent in e_dist[sent_idx]:
+                final_e1_dist.append(e1_dist_sent)
+                final_e2_dist.append(e2_dist_sent)
+
+    final_e1 = np.array(final_e1, dtype=np.int32)
+    final_e2 = np.array(final_e2, dtype=np.int32)
+    final_e1_dist = np.array(final_e1_dist, dtype=np.int32)
+    final_e2_dist = np.array(final_e2_dist, dtype=np.int32)
+    final_tokens = np.array(final_tokens, dtype=np.int32)
+    final_seq_len = np.array(final_seq_len, dtype=np.int32)
+    rel = np.zeros_like(final_e1)
+    ep = np.zeros_like(final_e1)
+    doc_ids = np.ones_like(final_e1) * pid
+
+    ep_dist = np.full((final_e1_dist.shape[0], final_e1_dist.shape[1], final_e1_dist.shape[1]), -1e8)
+    ep_indices = [(b, ei, ej) for b in range(final_e1_dist.shape[0])
+                  for ei in np.where(final_e1_dist[b] == 1)[0]
+                  for ej in np.where(final_e2_dist[b] == 1)[0]]
+    b, r, c = zip(*ep_indices)
+    ep_dist[b, r, c] = 0.0
+
+    pos_encode = [range(1, final_tokens.shape[1] + 1) for i in range(final_tokens.shape[0])]
+    label_batch = rel
+    ex_loss = np.ones_like(final_e1)
+
+    feed_dict = {model.text_batch: final_tokens,
+                 model.e1_dist_batch: final_e1_dist,
+                 model.e2_dist_batch: final_e2_dist,
+                 model.seq_len_batch: final_seq_len,
+                 model.label_batch: label_batch,
+                 model.pos_encode_batch: pos_encode,
+                 model.ep_batch: ep,
+                 model.kb_batch: rel,
+                 model.e1_batch: final_e1,
+                 model.e2_batch: final_e2,
+                 model.example_loss_weights: ex_loss,
+                 model.ep_dist_batch: ep_dist}
+
+    return feed_dict, final_e1.shape[0], doc_ids
+
+
+def make_example(text_list, seq_len, str_id_maps=None, tokenize=None, max_sent_len=200):
+    line_num = 0
+
+    line = text_list[line_num].strip()
+    pub_id, _, title_list = line.split('|', 2)
+    line_num += 1
+    title = ''.join(title_list)
+    abstract = ''.join(text_list[line_num].strip().split('|')[2:])
+    line_num += 1
+    current_pub = '%s %s' % (title, abstract)
+    label_annotations = {}
+    current_annotations = []
+    ner_label_str_id_map = str_id_maps['ner_label_str_id_map']
+    entity_str_id_map = str_id_maps['entity_str_id_map']
+
+    line = text_list[line_num].strip()
+    line_num += 1
+    while line_num <= len(text_list) and line:
+
+        parts = line.split('\t')
+        if len(parts) == 4:
+            pub_id, rel, e1, e2 = parts
+            label_annotations[(e1, e2, pub_id)] = rel
+        elif len(parts) == 6:
+            pub_id, start, end, mention, label, kg_ids = parts
+            if label in ['Chemical', 'Disease']:
+                for kg_id in kg_ids.split('|'):
+                    current_annotations.append((pub_id, int(start), int(end), mention, label, kg_id))
+        elif len(parts) == 7:
+            pub_id, start, end, mention, label, kg_ids, split_mentions = parts
+            if label in ['Chemical', 'Disease']:
+                for kg_id in kg_ids.split('|'):
+                    current_annotations.append((pub_id, int(start), int(end), mention, label, kg_id))
+
+        if line_num < len(text_list):
+            line = text_list[line_num].strip()
+        line_num += 1
+
+    # do something with last annotations
+    sorted_annotations = sorted(current_annotations, key=lambda tup: tup[1])
+    replaced_text = []
+    last = 0
+    annotation_map = {}
+    for i, (pub_id, start, end, mention, label, kg_id) in enumerate(sorted_annotations):
+        mention = current_pub[start:end]
+        dummy_token = '%s%d_' % (ENTITY_STRING, i)
+        replaced_text.append(' %s %s ' % (current_pub[last:start].strip(), dummy_token))
+        last = end
+        annotation_map[dummy_token] = (mention, label, kg_id)
+
+    # add text that occurs after the last entity
+    replaced_text.append(current_pub[end:])
+    abstract = ''.join(replaced_text).replace('  ', ' ')
+    sentences = sent_tokenize(abstract)
+    for sent in sentences:
+        print(len(sent))
+    tokenized_sentences = [[w for w in tokenize(s)] for s in sentences]
+
+    out_sentence = [[tokenize(annotation_map[token][0]) if token.startswith(ENTITY_STRING) else [token]
+                     for token in sentence]
+                    for sentence in tokenized_sentences]
+
+    # get the token offsets in the sentence for each entity
+    token_lens = [[len(token) if type(token) is list else 1 for token in sentence]
+                  for sentence in out_sentence]
+    token_offsets = [np.cumsum(lens) for lens in token_lens]
+
+    entity_offsets = [[(annotation_map[token], offset[i] - length[i], offset[i])
+                       for (i, token) in enumerate(sentence) if token.startswith(ENTITY_STRING)]
+                      for length, offset, sentence
+                      in zip(token_lens, token_offsets, tokenized_sentences)]
+    print(entity_offsets)
+    print(len(entity_offsets))
+
+    num_sentences = len(entity_offsets)
+    ner_labels = np.zeros((num_sentences, max_sent_len))
+    entities = np.zeros((num_sentences, max_sent_len))
+    e1_abst = []
+    e1_dist_abst = []
+
+    for sent_idx, sent_offsets in enumerate(entity_offsets):
+        print(sent_idx)
+        sent_len = seq_len[sent_idx]
+        token_idx = 0
+        prev_end = 0
+        e1_sent = []
+        e1_dist_sent = []
+
+        if len(sent_offsets) == 0:
+            ner_labels[sent_idx, :sent_len] = ner_label_str_id_map['O']
+            continue
+
+        for (mention, label, ent_str), start, end in sent_offsets:
+            e1_dist = np.zeros(max_sent_len, dtype=np.int32)
+            e1_dist[start:end] = 1
+            e1_dist_sent.append(e1_dist)
+
+            for _ in range(prev_end, start):
+                ner_labels[sent_idx, token_idx] = ner_label_str_id_map['O']
+                entities[sent_idx, token_idx] = 0
+                token_idx += 1
+
+            ner_labels[sent_idx, token_idx] = ner_label_str_id_map['B-' + label]
+
+            if ent_str not in entity_str_id_map:
+                mesh_str = 'MESH:' + ent_str
+                chebi_str = 'CHEBI:' + ent_str
+                if mesh_str in entity_str_id_map:
+                    ent_id = entity_str_id_map.get(mesh_str)
+                if chebi_str in entity_str_id_map:
+                    ent_id = entity_str_id_map.get(chebi_str)
+            else:
+                ent_id = entity_str_id_map.get(ent_str)
+
+            entities[sent_idx, token_idx] = ent_id
+            e1_sent.append(ent_id)
+
+            token_idx += 1
+            for _ in range(start + 1, end):
+                ner_labels[sent_idx, token_idx] = ner_label_str_id_map['I-' + label]
+                token_idx += 1
+            prev_end = end
+
+        for _ in range(end, sent_len):
+            ner_labels[sent_idx, token_idx] = ner_label_str_id_map['O']
+            token_idx += 1
+
+        e1_abst.append(e1_sent)
+        e1_dist_abst.append(e1_dist_sent)
+
+    return ner_labels, entities, e1_abst, e1_dist_abst
+
+
+def export_predictions(sess, model, FLAGS, pid, string_int_maps, threshold_map=None):
+    print('Evaluating')
+    null_label_set = set([int(l) for l in FLAGS.null_label.split(',')])
+
+    result_list = [model.probs, model.label_batch, model.e1_batch, model.e2_batch]
+
+    feed_dict, batch_size, doc_ids = parse_pubtator(pid)
+
+    probs, labels, e1, e2 = sess.run(result_list, feed_dict=feed_dict)
+    labeled_scores = [(l, np.argmax(s), np.max(s), _e1, _e2, did)
+                      for s, l, _e1, _e2, did in zip(probs, labels, e1, e2, doc_ids)]
+
+    final_out = []
+    for label_id in range(FLAGS.num_classes):
+        if label_id not in null_label_set:
+            label_str = string_int_maps['kb_id_str_map'][label_id]
+            threshold = threshold_map[label_id] if threshold_map \
+                else [float(t) for t in FLAGS.thresholds.split(',')]
+
+            # label, prediction, confidence
+            predictions = [(_e1, _e2, did) for label, pred, conf, _e1, _e2, did in labeled_scores
+                           if conf >= threshold and pred == label_id and _e1 != _e2]
+            mapped_predictions = [(string_int_maps['entity_id_str_map'][_e1],
+                                   string_int_maps['entity_id_str_map'][_e2], did)
+                                  for _e1, _e2, did in predictions]
+            out_lines = ['%s\t%s\tArg1:%s\tArg2:%s\n' % (did, label_str, _e1, _e2)
+                         for _e1, _e2, did in mapped_predictions]
+            final_out.extend(out_lines)
+
+    return set(final_out)
